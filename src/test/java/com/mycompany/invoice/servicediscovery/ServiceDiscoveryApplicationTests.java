de3d368696c94fd7ac03e50c47d9f3ccddcbd60c
ccml <claude.cato@proximus.be>
2022-06-19 16:55:57 +0200

    cloud

---
+++ src/test/java/com/mycompany/invoice/servicediscovery/ServiceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.invoice.servicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
